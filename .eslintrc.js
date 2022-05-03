fbd6306d81022664c4c5c58f5be480b65f5a55cf
Paul Wandrey <88623843+Kodeiak@users.noreply.github.com>
2022-05-03 11:00:44 -0500

    add eslint and create .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "commonjs": true,
+        "jquery": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    }
+};
